0ca061345c0a2ba6c3bbfdaeba9f3415eb3d27c0
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2024-09-03 19:03:16 +0200

    new responsivew

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -8,7 +8,6 @@ import Footer from "../Components/Footer";
 import "animate.css";
 import "../Styles/style_homescreen.css";
 /* Images */
-import logo from "../img/logo.png";
 import locationLogo from "../img/locationLogo.png";
 import alarmLogo from "../img/alarmLogo.png";
 import earth from "../img/earth.png";
@@ -28,17 +27,15 @@ const HomeScreen = () => {
       <section id="intro" className="container-data">
         <div id="title-logo-container">
           <div id="title-area">
-            <h1 id="title">Exact weather exactly for you.</h1>
-          </div>
-          <div id="logo-area">
-            <img src={logo} alt="Logo" id="logo" />
+            <h1 id="title">Exact weather 🌅<br />exactly for you</h1>
+            <h2>And know a lot of things</h2>
           </div>
         </div>
 
         <div className="mini-container">
           <div id="button-area">
             <button className="btn arrow-button">
-              <a id="see" href="#about-area">See more</a>
+              <a id="see" href="#about-area">Discover</a>
               <span className="arrow"></span>
             </button>
           </div>
@@ -46,41 +43,44 @@ const HomeScreen = () => {
       </section>
 
       <section id="about-area" className="container-data">
-        <div id="about">
-          <div id="wapa-title">
-            <h1 id="big-text">Stay up to date and know a lot of things.</h1>
-          </div>
+        <div>
+          <h1 id="features-title">Our Features</h1>
+          <h2 id="features-subtitle">Stay connect to see new features in future</h2>
         </div>
 
-        <div id="things" className="container-data">
-          <InfoBox
-            title="Weather in one click"
-            text="WAPA provides all the weather information you need in an instant. Simply enter your location and instantly discover essential data like temperature, pressure, humidity, wind speed, and much more."
-            imgSrc={locationLogo}
-            imgAlt="Location Logo"
-          />
-          <InfoBox
-            title="Galactic predictions"
-            text="Using data directly from NASA's API, our web app delivers advanced predictions with incredibly detailed insights about the sun and celestial events."
-            imgSrc={earth}
-            imgAlt="Earth"
-          />
-          <InfoBox
-            title="Customize your experience"
-            text="Sign in to add your favorite locations and see weather updates directly. Turn on weather notifications to stay prepared for any storm."
-            imgSrc={alarmLogo}
-            imgAlt="Alarm Logo"
-          />
+        <div id="things" className="info-box-container">
+          <div className="info-box-large">
+            <InfoBox
+              title="Weather in one click"
+              text="WAPA provides all the weather information you need in an instant. Simply enter your location and instantly discover essential data like temperature, pressure, humidity, wind speed, and much more."
+              imgSrc={locationLogo}
+              imgAlt="Location Logo"
+            />
+          </div>
+          <div className="info-box-small">
+            <InfoBox
+              title="Galactic predictions"
+              text="Using data directly from NASA's API, our web app delivers advanced predictions with incredibly detailed insights about natural events and the universe, like celestial events. You also can watch some daily photos about Mars and the Earth!"
+              //imgSrc={earth}
+              //imgAlt="Earth"
+            />
+            <InfoBox
+              title="Customize your experience"
+              text="Sign in to add your favorite locations (1-6) and see weather updates directly. Turn on weather notifications to stay prepared for any weather alarm like strange temperature or thunderstorm."
+              //imgSrc={alarmLogo}
+              //imgAlt="Alarm Logo"
+            />
+          </div>
         </div>
 
         <div id="call-to-action-area">
           <h2 id="call-to-action-text">🚀 Join WAPA Today! 🚀</h2>
           <h3 id="call-to-action-text2">
-            Become part of our community and gain instant access to your favorite places, advanced predictions, and customizable experience.<br></br>
+            Become part of our community and gain instant access to your favorite places, advanced predictions, and customizable experience.<br />
             💡 Don't wait! Your new WAPA experience starts here 💡
           </h3>
           <button id="call-to-action-btn">
-            <Link to="/SignUpScreen">JOIN US!</Link>
+            <Link to="/SignUpScreen">Join us!</Link>
           </button>
         </div>
       </section>
