14853d7ee562eb7c7115d7960d183ec4a721fa83
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2024-09-06 00:04:04 +0200

    logo
    ios and splash screen su android

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App'; // Import your App component
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// Registrazione del service worker per l'esperienza offline
+serviceWorkerRegistration.register();
+
 ReactDOM.render(
   <Router>
     <App />
