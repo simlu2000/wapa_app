0f7ddc589799a2a51e2137fb4fabdc921ea8760c
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2024-09-06 10:48:15 +0200

    update app js routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import { Route, Routes } from 'react-router-dom';
 import { auth } from './Utils/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 
-import SideBar from "../src/Components/Sidebar";
+import SideBar from "./Components/Sidebar";
 import Footer from "./Components/Footer";
 import HomeScreen from "./Screens/HomeScreen";
 import WeatherScreen from './Screens/WeatherScreen';
@@ -58,23 +58,21 @@ const App = () => {
     }, []);
 
     return (
-        <Router> {/* Aggiungi BrowserRouter qui */}
-            <div className='App'>
-                <SideBar user={user} />
-                <div className='main-content'>
-                    <Routes>
-                        <Route path='/' element={<HomeScreen />} />
-                        <Route path='/WeatherScreen' element={<WeatherScreen />} />
-                        <Route path='/AdvancedScreen' element={<AdvancedScreen />} />
-                        <Route path='/SignUpScreen' element={<SignUpScreen />} />
-                        <Route path='/PrivacyPolicesScreen' element={<PrivacyPolicesScreen />} />
-                        <Route path='/UserProfileScreen' element={<UserProfileScreen user={user} />} />
-                        <Route path='/AboutScreen' element={<AboutScreen />} />
-                    </Routes>
-                </div>
-                <Footer />
+        <div className='App'>
+            <SideBar user={user} />
+            <div className='main-content'>
+                <Routes>
+                    <Route path='/' element={<HomeScreen />} />
+                    <Route path='/WeatherScreen' element={<WeatherScreen />} />
+                    <Route path='/AdvancedScreen' element={<AdvancedScreen />} />
+                    <Route path='/SignUpScreen' element={<SignUpScreen />} />
+                    <Route path='/PrivacyPolicesScreen' element={<PrivacyPolicesScreen />} />
+                    <Route path='/UserProfileScreen' element={<UserProfileScreen user={user} />} />
+                    <Route path='/AboutScreen' element={<AboutScreen />} />
+                </Routes>
             </div>
-        </Router>
+            <Footer />
+        </div>
     );
 }
 
