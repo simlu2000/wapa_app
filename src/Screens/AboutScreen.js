b959111c71db61c1fb45a93a3b0e88f56c320e85
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2024-12-02 16:12:32 +0100

    about page

--- src/Screens/AboutScreen.js
+++ src/Screens/AboutScreen.js
@@ -2,17 +2,35 @@ import React from "react";
 import '../Styles/style_aboutscreen.css';
 
 const AboutScreen = () => {
-
-
     return (
         <>
+            <h1 className="info-title">About WAPA</h1>
             <section className="about-container">
-                <h1 id="wapa-title">About WAPA</h1>
+                <p>WAPA (Weather and Advanced Predictions App) è stata sviluppata per la mia tesi di laurea in Informatica e
+                    l'obiettivo principale era quello di creare un'applicazione web
+                    progressiva in grado di dare agli utenti informazioni basilari
+                    sul meteo e avanzate, mediante anche la rappresentazione con grafici.
+                    Per migliorare l'esperienza utente è stato implementato un sistema di
+                    personalizzazione automatica della schermata meteo in base alla condizione
+                    meteo corrente dell'utente o selezionata dall'utente.
+                </p>
             </section>
-            <section className="info-container">
-                
+
+            <section>
+                <h2 className="info-title">About the Developer</h2>
+                <div className="developer-card">
+                    <img src="path/to/your-photo.jpg" alt="Simone Lutero" className="developer-photo" />
+                    <div className="developer-info">
+                        <h3>Simone Lutero</h3>
+                        <p>Studente di Informatica e Full-stack Developer</p>
+                        <p><strong>Email:</strong>simone.lutero1@gmail.com</p>
+                    </div>
+                </div>
             </section>
+
+            
         </>
-    )
-}
+    );
+};
+
 export default AboutScreen;
