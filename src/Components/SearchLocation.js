6e2d358becfe899a42024d080c938dfb084f46f3
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2025-03-21 16:32:11 +0100

    Update SearchLocation.js

--- src/Components/SearchLocation.js
+++ src/Components/SearchLocation.js
@@ -1,48 +1,31 @@
-import React, { useState, useRef } from 'react';
-import { GoogleMap, LoadScript, Autocomplete } from '@react-google-maps/api';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-const libraries = ['places'];
-const Api_Key_GooglePlaces = process.env.REACT_APP_Api_Key_googlePlaces;
 
 const SearchLocation = ({ onSearch }) => {
     const [query, setQuery] = useState('');
-    const autocompleteRef = useRef(null);
 
-    const handlePlaceChanged = () => {
-        const place = autocompleteRef.current.getPlace();
-        if (place && place.formatted_address) {
-            setQuery(place.formatted_address);
-            if (onSearch) {
-                onSearch(place.formatted_address);
-            }
+    const handleSearch = (event) => {
+        event.preventDefault();
+        if (onSearch) {
+            onSearch(query);
         }
     };
 
     return (
-        <LoadScript
-            googleMapsApiKey={Api_Key_GooglePlaces}
-            libraries={libraries}
-        >
-            <form className="search-location-form" onSubmit={(e) => e.preventDefault()}>
-                <Autocomplete
-                    onLoad={(autocomplete) => (autocompleteRef.current = autocomplete)}
-                    onPlaceChanged={handlePlaceChanged}
-                >
-                    <input
-                        type="text"
-                        id="search"
-                        placeholder="Enter city..."
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                    />
-                </Autocomplete>
-                <button type="submit" className="search-button">
-                    <FontAwesomeIcon icon={faSearch} />
-                </button>
-            </form>
-        </LoadScript>
+        <form onSubmit={handleSearch} className="search-location-form">
+            <input
+                type="text"
+                id="search"
+                placeholder="Enter city..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">
+                <FontAwesomeIcon icon={faSearch} />
+            </button>
+        </form> 
     );
 };
 
