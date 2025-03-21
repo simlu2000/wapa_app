8a56515060f4c5e5c7d64584c5a8ed6f3e2da733
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2025-03-21 16:25:55 +0100

    Update WeatherScreen.js

--- src/Screens/WeatherScreen.js
+++ src/Screens/WeatherScreen.js
@@ -143,7 +143,7 @@ const WeatherScreen = () => {
             );
             setForecastData(forecastResponse.data.list);
         } catch (error) {
-            console.error("Error. Location not found.", error);
+            console.error("Error. Location not found", error);
         }
     };
 
