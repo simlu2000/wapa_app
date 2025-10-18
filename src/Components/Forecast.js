a2ca0c43aa7bc20d61e781a819eff50369278b47
simlu2000 <73953810+simlu2000@users.noreply.github.com>
2025-10-18 20:58:28 +0200

    updates

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -101,8 +101,6 @@ const Forecast = ({ forecast }) => {
             justifyContent: 'center',
             transition: 'background-image 0.5s ease-in-out',
             background: 'rgba(255, 255, 255, 0.87)',
-            backdropFilter: 'blur(20px) saturate(180%)',
-            webkitBackdropFilter: 'blur(20px) saturate(180%)',
             width:'97%',
             marginTop:'10%',
             boxShadow: '2px 2px 8px rgba(0, 0, 0, 0.3)'
--- src/Screens/WeatherScreen.js
+++ src/Screens/WeatherScreen.js
@@ -227,8 +227,6 @@ const WeatherScreen = () => {
                         <div className="charts-container" style={{
                             border: '1px solid rgba(255, 255, 255, 0.25)',
                             boxShadow: '0 8px 32px rgba(0, 0, 0, 0.1)',
-                            //background: 'rgba(255, 255, 255, 0.87)',
-
                         }}
                         >
                             <WindCharts windSpeed={weatherData.wind.speed} />
@@ -237,7 +235,7 @@ const WeatherScreen = () => {
                         </div>
 
                         <section className="meteo-box-container" style={{
-                            //background: 'rgba(255, 255, 255, 0.87)',
+                            background: 'rgba(255, 255, 255, 0.87)',
                             backdropFilter: 'blur(20px) saturate(180%)',
                             WebkitBackdropFilter: 'blur(20px) saturate(180%)',
                             border: '1px solid rgba(255, 255, 255, 0.25)',
